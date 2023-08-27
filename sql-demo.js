4e90edacdf2813d8f22c65c55cc10719e19493ad
Avraham Y. Kahana <avreimale@gmail.com>
2023-08-27 12:43:49 +0300

    working with ddl

--- sql-demo.js
+++ sql-demo.js
@@ -1,12 +1,30 @@
 //copied from https://sql.js.org/examples/GUI/index.html
-
-var execBtn = document.getElementById("btnSearch");
+var select = document.getElementById("dbSelect");
+var btnSearch = document.getElementById("btnSearch");
 var txtSearch = document.getElementById("txtSearch");
 var outputElm = document.getElementById('output');
 var errorElm = document.getElementById('error');
-var dbFileElm = document.getElementById('dbFile');
-var lblImportedFile = document.getElementById('lblImportedFile');
 var startingImgId;
+
+startingImgId = parseInt(select.value.split('-')[1].replace(/\.[^/.]+$/, ""));
+
+select.addEventListener("change", (e) => 
+{
+	startingImgId = parseInt(e.target.value.split('-')[1].replace(/\.[^/.]+$/, ""));
+	outputElm.innerHTML = "";
+});
+
+txtSearch.addEventListener("keyup", () => 
+{
+	if (!txtSearch.value)
+	{
+		btnSearch.setAttribute('disabled','disabled');
+	}
+	else
+	{
+		btnSearch.removeAttribute('disabled');
+	}
+});
 
 // Start the worker in which sql.js will run
 var worker = new Worker("worker.sql-wasm.js");
@@ -29,47 +47,6 @@ function noerror() {
 	errorElm.style.height = '0';
 }
 
-// Run a command in the database
-function execute(commands) {
-	tic();
-	worker.onmessage = function (event) {
-		var results = event.data.results;
-		toc("Executing SQL");
-		if (!results) {
-			error({message: event.data.error});
-			return;
-		}
-
-		tic();
-		outputElm.innerHTML = "";
-		if (results.length == 0)
-		{
-			outputElm.innerHTML = "No entries found for " + txtSearch.value + ". Try another term.";
-		}
-		else
-		{
-			outputElm.innerHTML = results[0].values.length + " entries found for " + txtSearch.value + ". <br />";
-		}
-
-		for (var i = 0; i < results.length; i++) {
-			outputElm.appendChild(tableCreate(results[i].columns, results[i].values));
-		}
-
-		//turn 1st column values into links
-		var tds = document.querySelectorAll('table tbody td:nth-child(1)');
-		for(var i = 0; i < tds.length; i++)
-		{
-			var imgId = startingImgId + parseInt(tds[i].textContent);
-			//webClient.DownloadFile($"https://images.nypl.org/index.php?id={imgId}&t=w", destinationFileName);
-			tds[i].innerHTML = `<a target="_blank" rel="noopener noreferrer" href='https://images.nypl.org/index.php?id=${imgId}&t=w'>` + tds[i].textContent + "</a>"
-		}
-
-		toc("Displaying results");
-	}
-	worker.postMessage({ action: 'exec', sql: commands });
-	outputElm.textContent = "Fetching results...";
-}
-
 // Create an HTML table
 var tableCreate = function () {
 	function valconcat(vals, tagName) {
@@ -87,23 +64,51 @@ var tableCreate = function () {
 	}
 }();
 
-// Execute the commands when the button is clicked
-function execEditorContents() {
-/*
-	var data = (async function()
-	{
-		let db = await getDBConnection();
-		let results = await db.all("SELECT p.Number as Page, l.Number as Line, w.Number as Word FROM Page p  JOIN Line l on p.Id = l.PageId JOIN Word w on w.LineId = l.Id WHERE w.Text = + '" + txtSearch.value  + "';");
-		await db.close();
-		return JSON(results);
-	})();
-	
-	return;
-*/
+btnSearch.addEventListener("click", () => 
+{
 	noerror()
-	execute("SELECT p.Number as Page, l.Number as Line, w.Number as Word FROM Page p  JOIN Line l on p.Id = l.PageId JOIN Word w on w.LineId = l.Id WHERE w.Text = + '" + txtSearch.value  + "';");
-}
-execBtn.addEventListener("click", execEditorContents, true);
+	//execute("SELECT p.Number as Page, l.Number as Line, w.Number as Word FROM Page p  JOIN Line l on p.Id = l.PageId JOIN Word w on w.LineId = l.Id WHERE w.Text = + '" + txtSearch.value  + "';");
+
+	var xhr = new XMLHttpRequest();
+	xhr.open('GET', "books/" + select.value + ".db", true);
+	xhr.responseType = 'arraybuffer';
+
+	xhr.onload = function(e) {
+		var uInt8Array = new Uint8Array(this.response);
+		var db = new SQL.Database(uInt8Array);
+		var data = db.exec("SELECT p.Number as Page, l.Number as Line, w.Number as Word FROM Page p  JOIN Line l on p.Id = l.PageId JOIN Word w on w.LineId = l.Id WHERE w.Text = + '" + txtSearch.value  + "';");
+		// contents is now [{columns:['col1','col2',...], values:[[first row], [second row], ...]}]
+
+		var bookName = select.value.split('-')[0];
+		var results = data;
+		toc("Executing SQL");
+		if (results && results.length == 0) {
+			error({message: "No entries found for '" + txtSearch.value + "' on " + bookName + " db. Try another term."});
+			return;
+		}
+
+		tic();
+		outputElm.innerHTML = "";
+		outputElm.innerHTML = results[0].values.length + " entries found for <b>" + txtSearch.value + "</b> on <u>" + bookName + "</u> book. <br /><br />";
+
+		for (var i = 0; i < results.length; i++) {
+			outputElm.appendChild(tableCreate(results[i].columns, results[i].values));
+		}
+
+		//turn 1st column values into links
+		var tds = document.querySelectorAll('table tbody td:nth-child(1)');
+		for(var i = 0; i < tds.length; i++)
+		{
+			var imgId = startingImgId + parseInt(tds[i].textContent);
+			//webClient.DownloadFile($"https://images.nypl.org/index.php?id={imgId}&t=w", destinationFileName);
+			tds[i].innerHTML = `<a target="_blank" rel="noopener noreferrer" href='https://images.nypl.org/index.php?id=${imgId}&t=w'>` + tds[i].textContent + "</a>"
+		}
+
+		toc("Displaying results");
+
+	};
+	xhr.send();
+}, true);
 
 // Performance measurement functions
 var tictime;
@@ -113,45 +118,3 @@ function toc(msg) {
 	var dt = performance.now() - tictime;
 	console.log((msg || 'toc') + ": " + dt + "ms");
 }
-
-// Load a db from a file
-dbFileElm.onchange = function () {
-	var f = dbFileElm.files[0];
-	var r = new FileReader();
-	r.onload = function () {
-		worker.onmessage = function () {
-			toc("Loading database from file");
-		};
-		tic();
-		try {
-			worker.postMessage({ action: 'open', buffer: r.result }, [r.result]);
-			btnSearch.removeAttribute('disabled');
-			lblImportedFile.textContent = f.name;
-			startingImgId = parseInt(dbFileElm.files[0].name.split('-')[1].replace(/\.[^/.]+$/, ""));
-			
-		}
-		catch (exception) {
-			worker.postMessage({ action: 'open', buffer: r.result });
-		}
-	}
-	r.readAsArrayBuffer(f);
-}
-
-/*
-var SQL = (async function()
-{
-	return await initSqlJs({
-		// Required to load the wasm binary asynchronously. Of course, you can host it wherever you want
-		// You can omit locateFile completely when running in node
-		locateFile: file => 'sql-wasm.wasm'
-	  });
-})();
-
-async function getDBConnection(){
-    const db = await sqlite.open({
-        filename: "/books/Secureni-56632812.db",
-        driver: sqlite3.Database
-    });
-    return db;
-}
-*/
